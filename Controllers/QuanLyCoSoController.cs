00a9568f39c9019c2dfe98b5b01e2bf85d7e09d2
Admin <Admin@DESKTOP-7G6EIS9>
2023-10-26 17:34:33 +0700

    coomit code

--- Controllers/QuanLyCoSoController.cs
+++ Controllers/QuanLyCoSoController.cs
@@ -51,18 +51,14 @@ namespace SuperbrainEmail.Controllers
             {
                 var cn = new ViewModelChiNhanh()
                 {
+                    id = item.id,
                     Ten = item.Ten,
                     Dienthoai = item.dienthoai,
                     Email = item.email1,
                     DateExpire = item.DateExpire.ToString("dd/MM/yyyy"),
                     DateTKOnline = item.DateTKOnline.ToString("dd/MM/yyyy"),
                     TrangthaiPhiTKOnline = Checktrangthai_TkOnline(item.id),
-                    SoluongTK = Calc_SoluongTK(item.id),
-                    SoluongTKPhatsinh = Calc_SoluongTKPhatsinh(item.id),
-                    PhiTK = Calc_phiTK(item.id),
-                    PhiTKPhatsinh = Calc_PhiTkPhatsinh(item.id),
-                    SoluongTKPhatsinhtruoc = Calc_SoluongTKTruoc(item.id),
-                    PhiTKPhatsinhtruoc = Calc_phiTKPhatsinhtruoc(item.id)
+                    SoluongTK = Calc_SoluongTK(item.id)
                 };
                 list.Add(cn);
             }
@@ -71,49 +67,50 @@ namespace SuperbrainEmail.Controllers
         }
         public ActionResult SendEmails()
         {
-            List<string> recipients = new List<string>
+            var conf = db.Configs.OrderByDescending(m => m.id);
+            ViewBag.date = conf.First().Thoigiantinhphi;
+            DateTime dateTinhphi = conf.First().Thoigiantinhphi.Value.AddMonths(1);
+            string thang = dateTinhphi.ToString("MM/yyyy");
+            var cn = db.ChiNhanhs.Where(m=>m.enable==true).ToList();
+            foreach (var item in cn)
             {
-                "sonhale57@gmail.com",
-                "sonhale57.data@gmail.com",
-                "sonhale57.aws@gmail.com",
-                "sonhale57.tech@gmail.com",
-                "sa.tech@gmail.com",
-                "sa.xbv@gmail.com",
-                "sa.ows@gmail.com",
-                // Thêm các địa chỉ email khác vào danh sách
-            };
-
-            foreach (var recipient in recipients)
-            {
-                //BackgroundJob.Enqueue(() => _emailService.SendEmail(recipient, "Chủ đề của email", "Nội dung của email"));
-                var jobData = new MyJobData
-                {
-                    Title = "Gửi email "+recipient,
-                    // Các thông tin khác về công việc
-                };
-
-                BackgroundJob.Enqueue<JobSendmails>(x => x.Execute(jobData,recipient,"Chủ đề mail","Nội dung email"));
+                BackgroundJob.Enqueue(() => _emailService.SendEmail(item.email1, "[SUPERBRAIN VIỆT NAM] THÔNG BÁO PHÍ THÁNG " + thang, bodyEmail(item.id)));
             }
+            return View();
+        }
+        public ActionResult Check_Email(int id)
+        {
+            TempData["body"] = bodyEmail(id);
             return View();
         }
         public string bodyEmail(int id) {
             var cn = db.ChiNhanhs.Find(id);
             var conf = db.Configs.OrderByDescending(m => m.id);
+
             DateTime dateTinhphi = conf.First().Thoigiantinhphi.Value.AddMonths(1);
             DateTime fr = conf.First().Thoigiantinhphi.Value.AddMonths(-1);
             DateTime to = conf.First().Thoigiantinhphi.Value;
-            int sotaikhoan = Calc_SoluongTK(id);
-            decimal dongiataikhoan =conf.First().Phitaikhoan;
-            decimal thanhtienphitaikhoan = dongiataikhoan * sotaikhoan;
 
-            decimal tongtien = thanhtienphitaikhoan + 5000000;
-            //double thue = tongtien * 8;
+            int sotaikhoan = Calc_SoluongTK(id);
+            int sothang = Calc_sothang(id);
+
+            decimal dongiataikhoan =conf.First().Phitaikhoan;
+            decimal dongiathuonghieu =conf.First().Phithuonghieu;
+
+
+            decimal thanhtienphitaikhoan = dongiataikhoan * sotaikhoan;
+            decimal thanhtienphithuonghieu = dongiathuonghieu * Calc_sothang(id);
+
+
+            decimal tongtien = thanhtienphitaikhoan + thanhtienphithuonghieu;
+            double thue = (double)tongtien * 0.08;
+            double tongthanhtoan = (double)tongtien + thue;
 
             string body = "<div style=\"display: block; padding: 20px; margin: 0 auto; font-family: 'Google Sans',Arial, roboto, sans-serif;\">"
                 + "<p>Kính gửi <b>" + cn.Ten + "</b>,</p>"
-                + "<p>Hệ thống đào tạo Superbrain Việt Nam trân trọng thông báo phí tháng " + dateTinhphi.ToString("MM/yyyy") + " của cơ sở " + cn.Ten + " như sau:</p>"
+                + "<p>Hệ thống đào tạo Superbrain trân trọng thông báo phí tháng " + dateTinhphi.ToString("MM/yyyy") + " của cơ sở " + cn.Ten + " như sau:</p>"
                 + "<table style='border-spacing: 0;'>"
-                + "    <tr style='background: #a6ce39;'>"
+                + "    <tr style='background: #a6ce39;color:white;'>"
                 + "        <td style='width:5%;text-align: center;border: 0.5px solid #888;padding: 7px;'>STT</td>"
                 + "        <td style='width:37%;text-align: center;border: 0.5px solid #888;padding: 7px;'>Nội dung</td>"
                 + "        <td style='width:15%;text-align: center;border: 0.5px solid #888;padding: 7px;'>Số lượng</td>"
@@ -123,9 +120,9 @@ namespace SuperbrainEmail.Controllers
                 + "    <tr>"
                 + "        <td style='text-align: center;border: 0.5px solid #888;padding: 7px;'>01</td>"
                 + "        <td style='text-align: center;border: 0.5px solid #888;padding: 7px;'>Phí sử dụng thương hiệu hàng tháng</td>"
-                + "        <td style='text-align: center;border: 0.5px solid #888;padding: 7px;'>1</td>"
-                + "        <td style='text-align: center;border: 0.5px solid #888;padding: 7px;'>5,000,000</td>"
-                + "        <td style='text-align: right;border: 0.5px solid #888;padding: 7px;'>5,000,000</td>"
+                + "        <td style='text-align: center;border: 0.5px solid #888;padding: 7px;'>"+sothang+"</td>"
+                + "        <td style='text-align: center;border: 0.5px solid #888;padding: 7px;'>"+ string.Format("{0:N00}", dongiathuonghieu) + "</td>"
+                + "        <td style='text-align: right; border: 0.5px solid #888;padding: 7px;'>"+ string.Format("{0:N00}", thanhtienphithuonghieu) + "</td>"
                 + "    </tr>"
                 + "    <tr>"
                 + "        <td style='text-align: center;border: 0.5px solid #888;padding: 7px;'>02</td>"
@@ -139,49 +136,23 @@ namespace SuperbrainEmail.Controllers
                 + "        <td style='text-align: right;border: 0.5px solid #888;padding: 7px;'>" + string.Format("{0:N00}", tongtien) + "</td>"
                 + "    </tr>"
                 + "    <tr>"
-                + "        <td style='text-align: left;border: 0.5px solid #888;padding: 7px;' colspan='4'>Thuế VAT</td>"
-                + "        <td style='text-align: right;border: 0.5px solid #888;padding: 7px;'>VAT</td>"
+                + "        <td style='text-align: left;border: 0.5px solid #888;padding: 7px;' colspan='4'>Thuế GTGT (8% VAT)</td>"
+                + "        <td style='text-align: right;border: 0.5px solid #888;padding: 7px;'>" + string.Format("{0:N00}", thue) + "</td>"
                 + "   </tr>"
                 + "    <tr>"
                 + "        <td style='text-align: left;border: 0.5px solid #888;padding: 7px;' colspan='4'>Tổng cộng thanh toán</td>"
-                + "        <td style='text-align: right;border: 0.5px solid #888;padding: 7px;'>TONG</td>"
+                + "        <td style='text-align: right;border: 0.5px solid #888;padding: 7px;'>"+ string.Format("{0:N00}", tongthanhtoan) + "</td>"
                 + "    </tr>"
                 + "</table>"
-                + "<i>(*) Phí tài khoản Online học viên được tính từ " + fr.ToString("dd/MM/yyyy") + " - " + to.ToString("dd/MM/yyyy") + ".</i>"
+                + "<i>(*) Phí tài khoản Online học viên được tính từ " + fr.ToString("dd/MM/yyyy") + " - " + to.AddDays(-1).ToString("dd/MM/yyyy") + ".</i>"
                 + "<p>Cảm ơn Quý cơ sở,</p>"
-                + "<p><i>Lưu ý: Thư điện tử này được gửi ra từ hệ thống tự động. Quý Cơ sở vui lòng không trả lời thư điện tử này.></p>"
+                + "<p><i>Lưu ý: Thư điện tử này được gửi ra từ hệ thống tự động. Quý Cơ sở vui lòng không trả lời thư điện tử này.</i></p>"
                 + "<p><i>Trong trường hợp Quý Cơ sở có bất kỳ thắc mắc hoặc yêu cầu nào liên quan đến Bảng phí, vui lòng liên hệ với Bộ phận Kế toán qua số Zalo 090 662 1800 để được hỗ trợ.</p>"
                 + "<p>Trân trọng,</p>"
                 + "<p>Superbrain Việt Nam</p>"
                 + "<img src='https://superbrain.edu.vn/wp-content/uploads/2021/10/bpkt.png' alt='' width='100%' height='auto'/>"
                 + "</div>";
             return body;
-        }
-        public int Calc_SoluongTKTruoc(int id)
-        {
-            var conf = db.Configs.OrderByDescending(m => m.id);
-            DateTime frdate = conf.First().Thoigiantinhphi.Value.AddMonths(-2);
-            DateTime todate = conf.First().Thoigiantinhphi.Value.AddMonths(-1);
-
-            var linq = from hs in db.Hocsinhs
-                       join lhj in db.Lophoc_join on hs.id equals lhj.idhocvien
-                       where lhj.fromdate >= frdate && lhj.fromdate < todate && lhj.updatetime > todate && hs.idChiNhanh == id
-                       select hs;
-
-            return linq.Count();
-        }
-        public double Calc_phiTKPhatsinhtruoc(int id)
-        {
-            var conf = db.Configs.OrderByDescending(m => m.id);
-            DateTime frdate = conf.First().Thoigiantinhphi.Value.AddMonths(-2);
-            DateTime todate = conf.First().Thoigiantinhphi.Value.AddMonths(-1);
-
-            var linq = from hs in db.Hocsinhs
-                       join lhj in db.Lophoc_join on hs.id equals lhj.idhocvien
-                       where lhj.fromdate >= frdate && lhj.fromdate < todate && lhj.updatetime > todate  && hs.idChiNhanh == id
-                       select hs;
-            double tong = (linq.Count() * 60000) + (linq.Count() * 60000 * 0.08);
-            return tong;
         }
         public int Calc_SoluongTK(int id) {
             var conf = db.Configs.OrderByDescending(m => m.id);
@@ -190,48 +161,40 @@ namespace SuperbrainEmail.Controllers
 
             var linq = from hs in db.Hocsinhs
                        join lhj in db.Lophoc_join on hs.id equals lhj.idhocvien
-                       where lhj.fromdate >= frdate && lhj.fromdate < todate && lhj.updatetime >= frdate && lhj.updatetime < todate && hs.idChiNhanh == id
-                       select hs;
+                       where lhj.fromdate >= frdate && lhj.fromdate < todate && hs.idChiNhanh == id
+                       select hs.id;
 
-            return linq.Count();
+            return linq.Distinct().Count();
         }
-        public double Calc_phiTK(int id) {
-            var conf = db.Configs.OrderByDescending(m => m.id);
-            DateTime frdate = conf.First().Thoigiantinhphi.Value.AddMonths(-1);
-            DateTime todate = conf.First().Thoigiantinhphi.Value;
+        public int Calc_sothang(int id) {
+            var cn = db.ChiNhanhs.Find(id);
+            if(cn.DateExpire > DateTime.Now)
+            {
+                if(cn.DateExpire.Month == DateTime.Now.Month)
+                {
+                    return 1; // Đến hạn thanh toán
+                }
+                else
+                {
+                    return 0; // Chưa đến hạn thanh toán
+                }
+            }
+            else if(cn.DateExpire == DateTime.Now) 
+            {
+                return 1; //Ngày hôm nay hết hạn phải đóng phí
 
-            var linq = from hs in db.Hocsinhs
-                       join lhj in db.Lophoc_join on hs.id equals lhj.idhocvien
-                       where lhj.fromdate >= frdate && lhj.fromdate < todate && lhj.updatetime >= frdate && lhj.updatetime < todate && hs.idChiNhanh == id
-                       select hs;
-            double tong = (linq.Count() * 60000) + (linq.Count() * 60000* 0.08);
-            return tong;
-        }
-        public int Calc_SoluongTKPhatsinh(int id)
-        {
-            var conf = db.Configs.OrderByDescending(m => m.id);
-            DateTime frdate = conf.First().Thoigiantinhphi.Value.AddMonths(-1);
-            DateTime todate = conf.First().Thoigiantinhphi.Value;
-
-            var linq = from hs in db.Hocsinhs
-                       join lhj in db.Lophoc_join on hs.id equals lhj.idhocvien
-                       where lhj.fromdate >= frdate && lhj.fromdate < todate && lhj.updatetime >= todate  && hs.idChiNhanh == id
-                       select hs;
-
-            return linq.Count();
-        }
-        public double Calc_PhiTkPhatsinh(int id)
-        {
-            var conf = db.Configs.OrderByDescending(m => m.id);
-            DateTime frdate = conf.First().Thoigiantinhphi.Value.AddMonths(-1);
-            DateTime todate = conf.First().Thoigiantinhphi.Value;
-
-            var linq = from hs in db.Hocsinhs
-                       join lhj in db.Lophoc_join on hs.id equals lhj.idhocvien
-                       where lhj.fromdate >= frdate && lhj.fromdate < todate && lhj.updatetime >= todate && hs.idChiNhanh == id
-                       select hs;
-            double tong = (linq.Count() * 60000) + (linq.Count() * 60000 * 0.08);
-            return tong;
+            }
+            else 
+            { 
+                if(cn.DateExpire.Year == DateTime.Now.Year)
+                {
+                    return DateTime.Now.Month - cn.DateExpire.Month;
+                }
+                else
+                {
+                    return Math.Abs((DateTime.Now.Month - cn.DateExpire.Month) + 12 * (DateTime.Now.Year - cn.DateExpire.Year));
+                }
+            }
         }
         public string Checktrangthai_TkOnline(int id)
         {
